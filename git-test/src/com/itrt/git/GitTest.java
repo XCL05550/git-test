df1b35b3b24e15c3e38949721e004532ad9a2fe2
XCL05550 <3466640880@qq.com>
2020-04-05 22:02:19 +0800

    GitTest功能增强

--- git-test/src/com/itrt/git/GitTest.java
+++ git-test/src/com/itrt/git/GitTest.java
@@ -10,4 +10,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
     }
+
+    public void test01(){
+        int i=10;
+        i++;
+    }
 }
