01bf87d5054c2bf6200888576dc003ca31cf8cc7
XCL05550 <3466640880@qq.com>
2020-04-05 21:58:37 +0800

    添加git-test项目到git本地仓库

---
+++ git-test/src/com/itrt/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.itrt.git;
+
+/**
+ * @author: XCL
+ * @Description: com.itrt.git
+ * @Date: 2020/4/5
+ * @ModifiedBy:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
